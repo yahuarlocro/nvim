6258d1f3027a99e1c68ac54a86293a6f15023ac0
yahuarlocro <yahuarlocro@tutanota.com>
2023-01-20 14:41:55 +0100

    add nvimtree and webicons

--- lua/yahuarlocro/packer.lua
+++ lua/yahuarlocro/packer.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function(use)
 
   -- harpoon
   use('theprimeagen/harpoon')
- 
+
   -- undotree
   use('mbbill/undotree')
 
@@ -31,28 +31,39 @@ return require('packer').startup(function(use)
 
   -- lsp zero
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  -- Snippet Collection (Optional)
-		  {'rafamadriz/friendly-snippets'},
-	  }
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          -- Snippet Collection (Optional)
+          {'rafamadriz/friendly-snippets'},
+      }
   }
 
+  -- nvim tree
+  use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+          'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      },
+      tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- devicons for nvim tree
+  use 'nvim-tree/nvim-web-devicons'
 
 
 end)
