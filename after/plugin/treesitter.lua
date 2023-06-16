920519f70bb63f1fd4200b6549bb81a2bff57d2e
yahuarlocro <yahuarlocro@tutanota.com>
2023-06-16 08:30:58 +0200

    remove unused languages in treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "rust", "python", "javascript", "php", "html", "css", "json", "regex", "twig", "terraform", "yaml", "markdown", "go" },
+  ensure_installed = { "python", "javascript", "html", "css", "json", "regex", "yaml", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
