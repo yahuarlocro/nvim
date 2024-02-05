b9308d4fb7f479bf1d0297a9cc45a02a7fdcd805
yahuarlocro <yahuarlocro@tutanota.com>
2024-02-05 11:11:02 +0100

    modified telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,6 +8,13 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      find_command = {
+        'rg', '--files', '--hidden', '-g', '!.git'
+      },
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
